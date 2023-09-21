2761f51d8f4770a3ed60eeeb14ff12d5eb3a2e61
nitinsuri <1166830+nitinsuri@users.noreply.github.com>
2023-09-21 13:16:31 +1000

    1 file modified

--- src/ComponentOneOne.jsx
+++ src/ComponentOneOne.jsx
@@ -1,16 +1,20 @@
-import  React, { useContext } from 'react';
-import { AppContext } from './AppContext'
+import React, { useContext } from 'react';
+import { AppContext } from './AppContext';
 
 export default function ComponentOneOne() {
-    const [countOne, setCountOne] = useContext(AppContext);
-    console.log('ComponentOneOne');
-    return (
-        <>
-            <h1>Hello from ComponentOne</h1>
-            <h2>Count: {countOne}</h2>
-            <button onClick={() => setCountOne(countOne => countOne + 1)}>Increment</button>
-            <button onClick={() => setCountOne(countOne => countOne - 1)}>Decrement</button>
-            <button onClick={() => setCountOne(0)}>Reset</button>
-        </>
-    );
+  const [countOne, setCountOne] = useContext(AppContext);
+  console.log('ComponentOneOne');
+  return (
+    <>
+      <h1>Hello from ComponentOne</h1>
+      <h2>Count: {countOne}</h2>
+      <button onClick={() => setCountOne((countOne) => countOne + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCountOne((countOne) => countOne - 1)}>
+        Decrement
+      </button>
+      <button onClick={() => setCountOne(10)}>Reset</button>
+    </>
+  );
 }
