e58a86ebcfd45e84dae7e6c934391d372b3cebb0
nitinsuri <git@stackblitz.com>
2024-10-17 13:07:13 +1100

    1 file modified

--- src/OptiName.jsx
+++ src/OptiName.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 export default function OptiName() {
-  const [firstName, setFirstName] = React.useState('');
-  const [lastName, setLastName] = React.useState('');
-  const [fullName, setFullName] = React.useState('');
-  React.useEffect(() => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+  useEffect(() => {
     setFullName(`${firstName} ${lastName}`);
   }, [firstName, lastName]);
 
@@ -20,8 +19,6 @@ export default function OptiName() {
         id="lastName"
         onChange={(e) => setLastName(e.target.value)}
       />
-      <br />
-      {firstName} {lastName}
       <br />
       {fullName}
     </>
